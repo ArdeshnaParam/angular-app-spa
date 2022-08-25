126713c2540e235a589705c6e9ca0b90dd47e5e8
ArdeshnaParam <ardeshna.param@gmail.com>
2022-08-25 22:39:38 +0530

    Test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
     ServerComponent,
     ServersComponent,
     SuccessAlertComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
